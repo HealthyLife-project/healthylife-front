a3e3f810638663521b7ede4787ac210d304b23dc
ihoek <ihoek679@naver.com>
2025-04-07 19:10:30 +0900

    feat : 채팅방 생성 버튼

--- frontend/src/features/CreateChat/index.tsx
+++ frontend/src/features/CreateChat/index.tsx
@@ -2,17 +2,32 @@ import { CreateChatStyle } from "./styled";
 import { useRouter } from "next/router";
 import clsx from "clsx";
 import { useState, useEffect } from "react";
-import axios from "axios";
 import { Formik, useFormik } from "formik";
 import { Input, Button } from "antd";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import api from "@/util/chek";
+import { useSelector } from "react-redux";
+import { RootState } from "@/redux/store";
 
 //Coponent
 
 //채팅방 생성 컴포넌트
 const CreateChat = () => {
+  //변수 선언
+  const tokenList = useSelector((state: RootState) => state.token.tokenList); //store 확인용 변수
+
   //useState
+  const [category, setCategory] = useState("");
+  const [id, setId] = useState(tokenList.id);
+
+  //useEffect
+  useEffect(() => {
+    //console.log(window.location.pathname);
+    let params = new URLSearchParams(window.location.search); //pathName person인지 pet인지 확인용
+    //console.log(params.get("category"));
+    setCategory(params.get("category")!);
+  }, []);
 
   const router = useRouter();
 
@@ -20,12 +35,20 @@ const CreateChat = () => {
     initialValues: {
       title: "",
     },
-    onSubmit: (values) => {
+    onSubmit: (values): void => {
       //폼 안에 버튼을 눌렀을 때 생기는 것
-      console.log("values", values);
-      router.push("/");
+      //console.log("values", values);
+      //router.push("/");
 
-      //api.post(`chat/`)
+      api
+        .post(`chat/${category}/create`, { id: id, title: values })
+        .then((res) => {
+          console.log("res", res.data);
+          router.push(`/${category}`);
+        })
+        .catch((error) => {
+          console.log("채팅방 생성 버튼 오류 : ", error);
+        });
     },
   });
 
