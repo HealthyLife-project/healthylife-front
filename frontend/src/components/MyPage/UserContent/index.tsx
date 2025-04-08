67dd0a559a35a48b30dae7c060d19b226f94fbab
ihoek <ihoek679@naver.com>
2025-04-08 15:07:26 +0900

    fix : 개인정보 오류 수정

--- frontend/src/components/MyPage/UserContent/index.tsx
+++ frontend/src/components/MyPage/UserContent/index.tsx
@@ -1,7 +1,6 @@
 import { UserContentStyle } from "./styled"; //스타일
 import clsx from "clsx";
 import api from "@/util/chek";
-import axios from "axios";
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { RootState } from "@/redux/store";
@@ -15,37 +14,46 @@ const UserContent = () => {
 
   //useState - userinfo
   const [name, setName] = useState(""); //이름
-  const [nickname, SetNickName] = useState(""); //닉네임
-  const [address, SetAddress] = useState(""); //주소
-  const [email, SetEmail] = useState(""); //이메일
-  const [gender, SetGender] = useState(""); //성별
-  const [phone, SetPhone] = useState(""); //전화번호
-  const [userid, SetUserid] = useState(""); //유저 아이디
-  const [id, setId] = useState(Number(tokenList.id));
+  const [nickname, setNickName] = useState(""); //닉네임
+  const [address, setAddress] = useState(""); //주소
+  const [email, setEmail] = useState(""); //이메일
+  const [gender, setGender] = useState(""); //성별
+  const [phone, setPhone] = useState(""); //전화번호
+  const [userid, setUserid] = useState(""); //유저 아이디
+  const [id, setId] = useState<number | undefined>(undefined);
 
   //useState - uesrhashtag
   const [hashTag, setHashTag] = useState([""]);
+
+  //새로고침 시 redux 타이밍을 맞추기 위한 useEffect
+  useEffect(() => {
+    if (tokenList?.id) {
+      setId(tokenList.id);
+    }
+  }, [tokenList]);
+
   //현재 로그인한 유저 정보 가져오기
   useEffect(() => {
+    if (!id) return;
+
     api.get(`/user/${id}`).then((res) => {
       setName(res.data.name);
-      SetNickName(res.data.nickname);
-      SetAddress(res.data.address);
-      SetEmail(res.data.email);
-      SetGender(res.data.gender);
-      SetPhone(res.data.phone);
-      SetUserid(res.data.userid);
+      setNickName(res.data.nickname);
+      setAddress(res.data.address);
+      setEmail(res.data.email);
+      setGender(res.data.gender);
+      setPhone(res.data.phone);
+      setUserid(res.data.userid);
     });
-  }, []);
-
+  }, [id]);
   //해당 유저에 해당하는 해시태그
-  axios({
-    method: "get",
-    url: `http://localhost:5001/hashtag/hash/7`,
-  }).then((res) => {
-    //console.log(res.data)
-    //setHashTag(res.data);
-  });
+  // axios({
+  //   method: "get",
+  //   url: `http://localhost:5001/hashtag/hash/7`,
+  // }).then((res) => {
+  //   //console.log(res.data)
+  //   //setHashTag(res.data);
+  // });
 
   return (
     <UserContentStyle className={clsx("main-wrap")}>
