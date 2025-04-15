8cf71b53bcbcf4cbbeae574f9210725664b64a41
ihoek <ihoek679@naver.com>
2025-04-15 12:51:48 +0900

    fix : 로그아웃 css 수정

--- frontend/src/features/MainPage/Drawer/index.tsx
+++ frontend/src/features/MainPage/Drawer/index.tsx
@@ -7,6 +7,8 @@ import { useDispatch, useSelector } from "react-redux";
 import { setTokenList } from "@/redux/redux";
 import { MoonOutlined, SunOutlined } from "@ant-design/icons";
 import { setTheme, selectTheme } from "@/redux/theme";
+import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
 
 //Drawer 컴포넌트
 const DrawerContainer = () => {
@@ -21,21 +23,42 @@ const DrawerContainer = () => {
   };
   // 로그아웃 버튼
   function handleLogout() {
-    try {
-      api.get("/auth/logout");
-      dispatch(
-        setTokenList({
-          token: {
-            name: "",
-            userid: "",
-            id: "",
-          },
-        })
-      ); // redux 초기화
-      router.push("/");
-    } catch (error) {
-      console.error("Logout failed:", error);
-    }
+    const MySwal = withReactContent(Swal);
+
+    MySwal.fire({
+      icon: "warning",
+      title: "정말로 로그아웃 하시겠습니까?",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes",
+      allowOutsideClick: false,
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire({
+          title: "다음에 만나요",
+          icon: "success",
+        }).then(() => {
+          try {
+            api.get("/auth/logout");
+            dispatch(
+              setTokenList({
+                token: {
+                  name: "",
+                  userid: "",
+                  id: "",
+                },
+              })
+            ); // redux 초기화
+            router.push("/");
+          } catch (error) {
+            console.error("Logout failed:", error);
+          }
+        });
+      }
+
+      // router.push("/login");
+    });
   }
 
   //onChange
